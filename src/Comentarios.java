39d36241dc2e35bc24abc7be2ab0f9e610a2d08a
Jorge <jorge.medi.08@gmail.com>
2025-11-12 14:17:06 -0600

    Tema variable var y comentarios

---
+++ src/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+
+    public static void main(String[] args){
+
+        //Declaración de variables
+        String canal, curso, proposito;
+
+        canal = "JorgeCL16";
+        curso = "Java desde 0";
+        proposito = "Aprender a usar el lenguaje Java";
+
+        System.out.println("Nombre del canal: " + canal);
+        System.out.println("Nombre del curso: " + curso);
+        System.out.println("Propósito: " + proposito);
+    }
+}
---
+++ src/Var_java.java
@@ -0,0 +1,11 @@
+public class Var_java {
+
+    public static void main(String[] args){
+
+        var nombre = "Curso de Java";
+        var numero = 7;
+        var cifra = 10.50;
+
+        System.out.println(cifra);
+    }
+}
