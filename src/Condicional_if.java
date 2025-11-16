989be13322c66b6ace5adc919c666f03aed7d753
Jorge <jorge.medi.08@gmail.com>
2025-11-16 15:22:53 -0600

    Condicional if

---
+++ src/Condicional_if.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Condicional_if {
+    public static void main(String[] args){
+
+        String nombre;
+        int edad;
+
+        nombre = JOptionPane.showInputDialog(null, "Ingresa tu nombre..");
+
+        String edad2 = JOptionPane.showInputDialog(null, "Ingresa tu edad..");
+        edad = Integer.parseInt(edad2);
+
+        //analizando datos
+
+        if (edad < 12){
+            JOptionPane.showMessageDialog(null, "Eres un niño");
+        } else if(edad > 12 && edad < 19) {
+           JOptionPane.showMessageDialog(null, "Eres un adolescente");
+        } else{
+          JOptionPane.showMessageDialog(null, "Eres un adulto");
+        }
+
+    }
+}
