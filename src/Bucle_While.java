f1c5bd97edc3d3369f74869dcd545d885a63e3bc
Jorge <jorge.medi.08@gmail.com>
2025-11-17 21:53:38 -0600

    Bucle do while

---
+++ src/Bucle_While.java
@@ -0,0 +1,17 @@
+public class Bucle_While {
+    public static void main(String[] args) {
+
+        int f = 0;
+
+        //bucle While
+        while (f < 5){
+            System.out.println("Vuelta no.:" + f);
+
+            if (f==3)
+                System.out.println("Soy la vuelta 3");
+
+            f++;
+        }
+
+    }
+}
