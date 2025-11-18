4aa098fdf70f64fd5bfca4c26c3845107c270c13
Jorge <jorge.medi.08@gmail.com>
2025-11-17 22:06:09 -0600

    Ejercicio tabla de multiplicar

--- src/Bucle_While.java
+++ src/Bucle_While.java
@@ -1,17 +1,38 @@
+import java.util.Scanner;
+
 public class Bucle_While {
     public static void main(String[] args) {
 
-        int f = 0;
-
+        //int f = 0;
+        //Ejemplo basico
         //bucle While
-        while (f < 5){
+        /*while (f < 5){
             System.out.println("Vuelta no.:" + f);
 
             if (f==3)
                 System.out.println("Soy la vuelta 3");
 
             f++;
+        }*/
+
+        //tabla de multiplicar
+
+        Scanner entrada = new Scanner(System.in);
+
+        int tabla = 0;
+        int i = 1;
+        int resultado = 0;
+
+        System.out.println("Escribe la tabla de multiplicar que quieras hacer");
+        tabla = entrada.nextInt();
+
+        while (i <= 10){
+            resultado = tabla * i;
+            System.out.println(tabla + "x" + i + "=" + resultado );
+            i++;
         }
+
+
 
     }
 }
