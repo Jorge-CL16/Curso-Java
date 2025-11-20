559d7fc1d0b90b1edcfe61f6651b3040f2ee12fa
Jorge <jorge.medi.08@gmail.com>
2025-11-19 20:17:49 -0600

    Ejercicio con bucle fot

---
+++ src/Bucle_For.java
@@ -0,0 +1,17 @@
+public class Bucle_For {
+
+    public static void main(String[] args){
+
+
+        //bucle ascendente
+        for (int i = 1; i <= 10; i++){
+            System.out.println("vuelta numero: " + i);
+        }
+
+        //bucle descendente
+        for (int j = 10; j >= 0; j--){
+            System.out.println("vuelta numero: " + j);
+        }
+        
+    }
+}
