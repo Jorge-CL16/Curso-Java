b51dac60791578db426d38bd4cbe9bf648c321f4
Jorge <jorge.medi.08@gmail.com>
2025-12-04 20:21:32 -0600

    Operaciones con arrays

---
+++ src/Arreglos_operaciones.java
@@ -0,0 +1,18 @@
+public class Arreglos_operaciones {
+    public static void main(String[] args) {
+
+        int ventas1[] = {500,600,400,100,650};
+        int ventas2[] = {600,800,500,350,900};
+
+        int total[] = new int[ventas1.length];
+        int suma = 0;
+
+        System.out.println("Suma de valores");
+        for (int i = 0; i < ventas1.length; i++){
+            total[i] = ventas1[i] + ventas2[i];
+            suma = suma + total[i];
+            System.out.println(total[i]);
+        }
+        System.out.println("Suma total de todos los elementos: " + suma);
+    }
+}
