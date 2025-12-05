fd36e35e7b05f31ea8d6ef4f28c2ece0fecf2e26
Jorge <jorge.medi.08@gmail.com>
2025-12-04 20:11:47 -0600

    Recorrer arreglos con bucles

---
+++ src/Arreglos_bucles.java
@@ -0,0 +1,17 @@
+public class Arreglos_bucles   {
+    public static void main(String[] args){
+        String[] productos = {"","Martirllo",
+                "Destornillador",
+                "Taladro",
+                "LLave inglesa",
+                "Pinzas"  };
+
+        for(int i = 1; i < productos.length; i++){
+            System.out.println("Producto: " + i + "."+ productos[i]);
+        }
+
+        for (String herraminetas : productos){
+            System.out.println(herraminetas);
+        }
+    }
+}
